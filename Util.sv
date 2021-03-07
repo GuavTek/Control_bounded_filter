578feaacba4eeac9c83f2b06225d3983fe132d9d
GuavTek <36814224+GuavTek@users.noreply.github.com>
2021-03-08 00:05:13 +0100

    Update Util.sv
    Added constants for alu operation codes

--- Util.sv
+++ Util.sv
@@ -4,3 +4,7 @@ typedef struct packed {
     logic[15:0] r;
     logic[31:16] i;
 } complex;
+
+const logic[1:0] ALU_ADD = 2'b00;
+const logic[1:0] ALU_SUB = 2'b01;
+const logic[1:0] ALU_MULT = 2'b10;
