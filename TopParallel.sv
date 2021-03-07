14d1cc5483398fe78817ad38fc37501ba35b2bb1
GuavTek <36814224+GuavTek@users.noreply.github.com>
2021-03-08 00:05:32 +0100

    Update TopParallel.sv

--- TopParallel.sv
+++ TopParallel.sv
@@ -1,8 +1,10 @@
-
+`include "Util.sv"
+`include "RecursionModule.sv"
+`include "LUT.vhd"
 
 module Parallel_top #(
     parameter stages = 32,
-    parameter width = 32,
+    parameter width = 16,
     parameter N = 3
 ) (
     input logic [N-1:0] in,
