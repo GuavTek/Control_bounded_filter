2f52524538a1f74d0a46f4dd3d551e122e527c51
GuavTek <36814224+GuavTek@users.noreply.github.com>
2021-04-01 16:01:32 +0200

    Initial testbench

--- Stratus/Batch_filter/tb.cpp
+++ Stratus/Batch_filter/tb.cpp
@@ -2,6 +2,10 @@
 
 #include "tb.h"
 
+#define N 3
+#define SAMPLES_LENGTH 32767
+
+sc_uint <1> samples[N][SAMPLES_LENGTH]
 
 // Source thread
 void tb::source()
@@ -11,13 +15,51 @@ void tb::source()
     rst = 0;
     wait(2);
     rst = 1;
-    wait();
+//    wait();
     
+    // Load stimuli
+    std::ifstream ins("hardware_signals.csv");
+    if(ins.is_open()){
+        int x = 0;
+        int y = 0;
+        while(!ins.eof()){
+            if(x >= SAMPLES_LENGTH){
+                x = 0;
+                y++;
+            }
+            if(y >= N){
+                printf("Warning, stimuli outside buffer range");
+                // error?
+            }
+            int buf = 99;
+            ins >> buf;
+            if (buf == 1){
+                samples[y][x] = 1;
+                x++;
+            } else if (buf == 0){
+                samples[y][x] = 0;
+                x++;
+            }
+        }
+        ins.close();
+    } else {
+        printf("Failed to open stimuli file");
+        // Throw error
+    }
+
     // Write a set of values to the dut
-    for (int i = 0; i < 10; i++)
+    for (int i = 0; i < SAMPLES_LENGTH; i++)
     {
         // Write values to the DUT
+        Batch_filter_INPUT_DT stimuli[N]
+        for (int n = 0; n < N; n++)
+        {
+            stimuli.Samples[n] = samples[n][i];
+        }
+        outputs.put(stimuli);
     }
+
+    wait();
 }
 
 // Read all the expected values from the design
@@ -26,13 +68,24 @@ void tb::sink()
     inputs.reset();
     wait();                     // to synchronize with reset
     
-    
-    for (int i = 0; i < 10; i++)
-    {
-        // Read values from the DUT
+    // Discard initial empty results.
+    while(inputs.Result == 0.0);
+
+    std::ofstream ros("Results.csv");
+    if(!ros.is_open()){
+        printf("Error! Could not open output file");
     }
+    for (int i = 0; i < SAMPLES_LENGTH; i++)
+    {
+        if(i > 0){
+            ros << " ,";
+        }
+        // Read values from the DUT
+        Batch_filter_OUTPUT_DT val = inputs.get();
+        ros << val.Result;
+    }
+    ros.close();
     
-    printf("NOTE: You need to add code to the tb.cpp file in order to get a working simulation.\n");
     
     esc_stop();
 }
