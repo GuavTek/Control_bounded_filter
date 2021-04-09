dda9bcd48066755f37a97169b6a2924191549439
GuavTek <36814224+GuavTek@users.noreply.github.com>
2021-04-09 21:05:57 +0200

    Added dual port RAM

--- RAM.sv
+++ RAM.sv
@@ -1,10 +1,11 @@
-module RAM_single #(parameter 	depth = 32,
-								d_width = 3,
-								a_width = 5) (
-								input int addr,
-								input logic clk, write, 
-								inout logic[width-1:0] data
-								);
+module RAM_single #(
+	parameter 	depth = 32,
+	d_width = 3
+	) (
+	input logic[$clog2(depth)-1:0]  addr,
+	input logic clk, write, 
+	inout logic[width-1:0] data
+);
 	logic[d_width-1:0] mem[depth-1:0];
 	always @(posedge clk) begin
         if (write)
@@ -12,4 +13,27 @@ module RAM_single #(parameter 	depth = 32,
         else
 			data <= mem[addr];
    	end
+endmodule
+
+module RAM_dual #(
+	parameter 	depth = 32,
+  	d_width=3
+	) (
+  	input logic[$clog2(depth)-1:0] addr1, addr2,
+  	input logic write1,
+  	input logic clk,
+  	inout logic[d_width-1:0] data1, data2
+);
+    
+	logic[d_width-1:0] mem [depth-1:0];
+    
+	always @(posedge clk) begin
+    	if (write1) 
+			mem[addr1] <= data1;
+		else
+			data1 <= mem[addr1];
+
+		data2 <= mem[addr2];
+  	end
+        
 endmodule
