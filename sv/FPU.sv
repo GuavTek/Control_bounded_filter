091a8143779089f053f53d179dcef191ba21d848
GuavTek <ddjava@live.no>
2021-09-24 13:19:52 +0200

    Fix simulation errors

--- sv/FPU.sv
+++ sv/FPU.sv
@@ -1,34 +1,38 @@
 `ifndef FPU_SV_
 `define FPU_SV_
 
+`include "HardFloat-1/source/HardFloat_consts.vi"
+`include "HardFloat-1/source/HardFloat_primitives.v"
+`include "HardFloat-1/source/isSigNaNRecFN.v"
+`include "HardFloat-1/source/HardFloat_rawFN.v"
 `include "HardFloat-1/source/fNToRecFN.v"
 `include "HardFloat-1/source/recFNToFN.v"
 `include "HardFloat-1/source/addRecFN.v"
 `include "HardFloat-1/source/mulRecFN.v"
-`include "HardFloat-1/source/HardFloat_consts.vi"
 `include "Util.sv"
 
 module FPU #(parameter FPU_opcode op = ADD) (
     input floatType A, B,
     output floatType result
 );
-    const int mant_width = $bits(result.mantis);
-    const int exp_width = $bits(result.exp);
-    logic[(mant_width + exp_width):0] s1, s2, r1;
+    logic clk;  // dummy for property checker
+    logic[(`MANT_W + `EXP_W + 1):0] s1, s2, r1;
 
-    fNToRecFN #(.expWidth(exp_width), .sigWidth(mant_width+1)) aConv (.in(A), .out(s1));
-    fNToRecFN #(.expWidth(exp_width), .sigWidth(mant_width+1)) bConv (.in(B), .out(s2));
-    recFNToFN #(.expWidth(exp_width), .sigWidth(mant_width+1)) resConv (.in(r1), .out(result));
+    fNToRecFN #(.expWidth(`EXP_W), .sigWidth(`MANT_W+1)) aConv (.in(A), .out(s1));
+    fNToRecFN #(.expWidth(`EXP_W), .sigWidth(`MANT_W+1)) bConv (.in(B), .out(s2));
+    recFNToFN #(.expWidth(`EXP_W), .sigWidth(`MANT_W+1)) resConv (.in(r1), .out(result));
 
     generate
         case (op)
         ADD:
         begin
-            addRecFN #(.expWidth(exp_width), .sigWidth(mant_width+1)) add1 (.control(flControl_tininessAfterRounding), .subOp(0), .a(s1), .b(s2), .roundingMode(round_near_even), .out(r1));
+            logic[4:0] dummy;
+            addRecFN #(.expWidth(`EXP_W), .sigWidth(`MANT_W+1)) add1 (.control(`flControl_tininessAfterRounding), .subOp(1'b0), .a(s1), .b(s2), .roundingMode(`round_near_even), .out(r1), .exceptionFlags(dummy));
         end
         MULT:
         begin 
-            mulRecFN #(.expWidth(exp_width), .sigWidth(mant_width+1)) mul1 (.control(1), .a(s1), .b(s2), .roundingMode(round_near_even), .out(r1));
+            logic[4:0] dummy;
+            mulRecFN #(.expWidth(`EXP_W), .sigWidth(`MANT_W+1)) mul1 (.control(`flControl_tininessAfterRounding), .a(s1), .b(s2), .roundingMode(`round_near_even), .out(r1), .exceptionFlags(dummy));
         end
         endcase
     endgenerate
