d6b98072bf60d5e9222c54a854267d549a62e261
GuavTek <ddjava@live.no>
2021-09-28 18:24:03 +0200

    Make second port output only

--- sv/RAM.sv
+++ sv/RAM.sv
@@ -38,11 +38,12 @@ module RAM_dual #(
 	input logic[$clog2(depth)-1:0] addr1, addr2;
   	input logic write1;
   	input logic clk;
-  	inout logic[d_width-1:0] data1, data2;
+  	inout logic[d_width-1:0] data1;
+	output logic[d_width-1:0] data2;
 `ifdef INCLUDE_RAM
 	logic[d_width-1:0] mem [depth-1:0] = '{depth{0}};
     assign data1 = write1 ? 'bz : mem[addr1];
-	assign data2 = write1 ? 'bz : mem[addr2];
+	assign data2 = mem[addr2]; //write1 ? 'bz : mem[addr2];
 
 	always @(posedge clk) begin
     	if (write1) 
