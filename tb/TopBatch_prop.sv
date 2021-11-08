44f67de796c4375679c780a4954ec98a3fc12fe1
GuavTek <ddjava@live.no>
2021-11-08 13:44:41 +0100

    Update timings

--- tb/TopBatch_prop.sv
+++ tb/TopBatch_prop.sv
@@ -36,7 +36,7 @@ module Batch_top_prop #(
     property lookahead_p;
         int delay;
         logic[N*OSR-1:0] inSample;
-        1 |-> (1, delay=dBatCount) ##0 (1, inSample=inShift) ##[0:DownSampleDepth] !cyclePulse ##[1:DownSampleDepth] (delay==(dBatCountRev)) ##0 (1, lookahead_a(inSample));
+        1 |-> (1, delay=dBatCount) ##0 (1, inSample=inShift) ##[0:DownSampleDepth+1] !cyclePulse ##[1:DownSampleDepth+1] (delay==(dBatCountRev)) ##0 (1, lookahead_a(inSample));
     endproperty
 
     task automatic meanB_a(input logic[N*OSR-1:0] inSample);
@@ -47,7 +47,7 @@ module Batch_top_prop #(
     property meanB_p;
         int delay;
         logic[N*OSR-1:0] inSample;
-        1 |-> (1, delay=dBatCount) ##0 (1, inSample=inShift) ##[2*DownSampleDepth:3*DownSampleDepth] !cyclePulse ##[1:DownSampleDepth] (delay==dBatCountRev) ##0 (1, meanB_a(inSample));
+        1 |-> (1, delay=dBatCount) ##0 (1, inSample=inShift) ##[2*DownSampleDepth:3*DownSampleDepth+1] !cyclePulse ##[1:DownSampleDepth+1] (delay==dBatCountRev) ##0 (1, meanB_a(inSample));
     endproperty
 
     task automatic meanF_a(input logic[N*OSR-1:0] inSample);
@@ -58,7 +58,7 @@ module Batch_top_prop #(
     property meanF_p;
         int delay;
         logic[N*OSR-1:0] inSample;
-        1 |-> (1, delay=dBatCount) ##0 (1, inSample=inShift) ##[2*DownSampleDepth:3*DownSampleDepth] !cyclePulse ##[1:DownSampleDepth] (delay==dBatCount) ##0 (1, meanF_a(inSample));
+        1 |-> (1, delay=dBatCount) ##0 (1, inSample=inShift) ##[2*DownSampleDepth:3*DownSampleDepth+1] !cyclePulse ##[1:DownSampleDepth+1] (delay==dBatCount) ##0 (1, meanF_a(inSample));
     endproperty
 
     task automatic calc_a(input logic[N*OSR-1:0] sampleB, sampleF);
@@ -71,7 +71,7 @@ module Batch_top_prop #(
     property calc_p;
         int delay;
         logic[N*OSR-1:0] sampleB, sampleF;
-        (dBatCountRev >= (DownSampleDepth/2)) |-> (1, delay=dBatCount) ##0 (1, sampleB=scob) ##0 (1, sampleF=sf_delay)  ##[0:DownSampleDepth] (delay==dBatCountRev) ##0 (1, calc_a(sampleB, sampleF));
+        (dBatCountRev >= (DownSampleDepth/2)) |-> (1, delay=dBatCount) ##0 (1, sampleB=scob) ##0 (1, sampleF=sf_delay)  ##[0:DownSampleDepth+1] (delay==dBatCountRev) ##0 (1, calc_a(sampleB, sampleF));
     endproperty
 
     task automatic resF_a(input floatType resultSample);
@@ -82,7 +82,7 @@ module Batch_top_prop #(
     property resF_p;
         int delay;
         floatType resSample;
-        1 |-> (1, delay=delayBatCount[2]) ##0 (1, resSample=partMemF) ##[0:DownSampleDepth] (delay==delayBatCount[1]) ##1 (1, resF_a(resSample));
+        1 |-> (1, delay=delayBatCount[2]) ##0 (1, resSample=partMemF) ##[0:DownSampleDepth+1] (delay==delayBatCount[1]) ##1 (1, resF_a(resSample));
     endproperty
 
     task automatic resB_a(input floatType resultSample);
@@ -93,28 +93,28 @@ module Batch_top_prop #(
     property resB_p;
         int delay;
         floatType resSample;
-        1 |-> (1, delay=delayBatCount[2]) ##0 (1, resSample=partMemB) ##[0:DownSampleDepth] !$stable(delayCycle[1]) ##[0:DownSampleDepth] (delay==delayBatCountRev[1]) ##1 (1, resB_a(resSample));
+        1 |-> (1, delay=delayBatCount[2]) ##0 (1, resSample=partMemB) ##[0:DownSampleDepth+1] !$stable(delayCycle[1]) ##[0:DownSampleDepth+1] (delay==delayBatCountRev[1]) ##1 (1, resB_a(resSample));
     endproperty
 
-    assert property (@(posedge !clkDS) disable iff(!rst) lookahead_p)
+    assert property (@(negedge clkDS) disable iff(!rst) lookahead_p)
     else $warning("Ooops! assertion failed");
 
-    assert property (@(posedge !clkDS) disable iff(!rst) meanB_p)
+    assert property (@(negedge clkDS) disable iff(!rst) meanB_p)
     else $warning("Ooops! assertion failed");
 
-    assert property (@(posedge !clkDS) disable iff(!rst) meanF_p)
+    assert property (@(negedge clkDS) disable iff(!rst) meanF_p)
     else $warning("Ooops! assertion failed");
     
-    assert property (@(posedge !clkDS) disable iff(!rst) calc_p)
+    assert property (@(negedge clkDS) disable iff(!rst) calc_p)
     else $warning("Ooops! assertion failed");
 
-    assert property (@(posedge !clkDS) disable iff(!rst) resF_p)
+    assert property (@(negedge clkDS) disable iff(!rst) resF_p)
     else $warning("Ooops! assertion failed");
 
-    assert property (@(posedge !clkDS) disable iff(!rst) resB_p)
+    assert property (@(negedge clkDS) disable iff(!rst) resB_p)
     else $warning("Ooops! assertion failed");
 
-    assert property (@(posedge !clkDS) disable iff(!rst) continuity_p)
+    assert property (@(negedge clkDS) disable iff(!rst) continuity_p)
     else $warning("Potential discontinuity!! Output changed by %f, at cycle %1d index %4d", absr(ftor(finResult)) - absr(ftor($past(finResult))), delayCycle[1], delayBatCount[1] );
 
 endmodule
