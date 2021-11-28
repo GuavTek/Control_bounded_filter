bc55b4394ca1cb16a1ef3ddecf3eb9fe39388b79
GuavTek <ddjava@live.no>
2021-11-28 23:13:26 +0100

    Update FIR floatingpoint implementation Use Coefficients_Fx because datatype real is buggy in synthesis. Fix output format Carry over fixes from fixedpoint version.

--- tb/TB_FIR.sv
+++ tb/TB_FIR.sv
@@ -12,9 +12,9 @@
 `include "../sv/TopFIR.sv"
 `include "../sv/Util.sv"
 `include "../sv/FPU.sv"
-`include "FPU_prop.sv"
-`include "LUT_prop.sv"
-`include "TopFIR_prop.sv"
+//`include "FPU_prop.sv"
+//`include "LUT_prop.sv"
+//`include "TopFIR_prop.sv"
 
 `include "../sv/Data/Coefficients.sv"
 `define TestLength 24000
@@ -61,7 +61,7 @@ module TB_FIR #() ();
         end
         
         // Prepare first sample
-        $fscanf(fdi, "%b,\n", inSample);
+        void'($fscanf(fdi, "%b,\n", inSample));
 
         // Wait for reset cycle
         @(negedge rst);
@@ -86,7 +86,9 @@ module TB_FIR #() ();
     end
 
     // Write output file
-    floatType dutResult;
+    logic[`OUT_WIDTH-1:0] dutResult;
+    logic signed[`OUT_WIDTH-1:0] signedResult;
+    logic isValid;
     initial begin
         // Open output file
         static string file_path = {"./Data/", `STRINGIFY(`OUT_FILE), ".csv"};
@@ -100,6 +102,9 @@ module TB_FIR #() ();
         @(negedge rst);
         @(posedge rst);
 
+        // Wait for valid data from dut
+        @(posedge isValid);
+
         // Write data
         for (int i = 0; i < `TestLength; i++) begin
             // Write only one in OSR number of results
@@ -107,7 +112,8 @@ module TB_FIR #() ();
                 @(posedge clk);
                 continue;
             end
-            $fwrite(fdo, "%f, ", ftor(dutResult));
+            signedResult = {~dutResult[`OUT_WIDTH-1], dutResult[`OUT_WIDTH-2:0]};
+            $fwrite(fdo, "%f, ", signedResult);
             //$fwrite(fdo, "%b;\n", result);
             if (`VERBOSE_LVL > 2)
                 $display("Write result %d.\n", i);
@@ -120,6 +126,66 @@ module TB_FIR #() ();
         // End simulation
         $finish;
     end
+
+    typedef struct packed { 
+        logic sign; 
+        logic[8-1:0] exp;
+        logic[15-1:0] mant;
+    } float_t;
+
+    /*
+    // Classes are really buggy
+    //convert#(.n_int(16), .n_mant(48), .f_exp(8), .f_mant(15)) convert_c();
+
+    float_t tempResult;
+    real reCon;
+    initial begin
+        $display("Case 0.0:");
+        tempResult = convert_nonsynth#(.n_int(16), .n_mant(48), .f_exp(8), .f_mant(15))::rtof(0.0);
+        reCon = convert_nonsynth#(.n_int(16), .n_mant(48), .f_exp(8), .f_mant(15))::ftor(tempResult);
+        $display("Dummy test: sign %1b exp %d mant %h = %f", tempResult.sign, tempResult.exp, tempResult.mant, reCon);
+        
+        tempResult = convert#(.n_int(16), .n_mant(48), .f_exp(8), .f_mant(15))::itof(0);
+        reCon = convert_nonsynth#(.n_int(16), .n_mant(48), .f_exp(8), .f_mant(15))::ftor(tempResult);
+        $display("Float: sign %1b exp %d mant %h = %f", tempResult.sign, tempResult.exp, tempResult.mant, reCon);
+
+        $display("Case 1.5:");
+        tempResult = convert_nonsynth#(.n_int(16), .n_mant(48), .f_exp(8), .f_mant(15))::rtof(1.5);
+        reCon = convert_nonsynth#(.n_int(16), .n_mant(48), .f_exp(8), .f_mant(15))::ftor(tempResult);
+        $display("Dummy test: sign %1b exp %d mant %h = %f", tempResult.sign, tempResult.exp, tempResult.mant, reCon);
+        
+        tempResult = convert#(.n_int(16), .n_mant(48), .f_exp(8), .f_mant(15))::itof('b11 << 47);
+        reCon = convert_nonsynth#(.n_int(16), .n_mant(48), .f_exp(8), .f_mant(15))::ftor(tempResult);
+        $display("Float: sign %1b exp %d mant %h = %f", tempResult.sign, tempResult.exp, tempResult.mant, reCon);
+
+        $display("Case 0.5:");
+        tempResult = convert_nonsynth#(.n_int(16), .n_mant(48), .f_exp(8), .f_mant(15))::rtof(0.5);
+        reCon = convert_nonsynth#(.n_int(16), .n_mant(48), .f_exp(8), .f_mant(15))::ftor(tempResult);
+        $display("Dummy test: sign %1b exp %d mant %h = %f", tempResult.sign, tempResult.exp, tempResult.mant, reCon);
+        
+        tempResult = convert#(.n_int(16), .n_mant(48), .f_exp(8), .f_mant(15))::itof(1 << 47);
+        reCon = convert_nonsynth#(.n_int(16), .n_mant(48), .f_exp(8), .f_mant(15))::ftor(tempResult);
+        $display("Float: sign %1b exp %d mant %h = %f", tempResult.sign, tempResult.exp, tempResult.mant, reCon);
+
+        $display("Case 127:");
+        tempResult = convert_nonsynth#(.n_int(16), .n_mant(48), .f_exp(8), .f_mant(15))::rtof(127);
+        reCon = convert_nonsynth#(.n_int(16), .n_mant(48), .f_exp(8), .f_mant(15))::ftor(tempResult);
+        $display("Dummy test: sign %1b exp %d mant %h = %f", tempResult.sign, tempResult.exp, tempResult.mant, reCon);
+        
+        tempResult = convert#(.n_int(16), .n_mant(48), .f_exp(8), .f_mant(15))::itof(127 << 48);
+        reCon = convert_nonsynth#(.n_int(16), .n_mant(48), .f_exp(8), .f_mant(15))::ftor(tempResult);
+        $display("Float: sign %1b exp %d mant %h = %f", tempResult.sign, tempResult.exp, tempResult.mant, reCon);
+
+        $display("Case -0.125:");
+        tempResult = convert_nonsynth#(.n_int(16), .n_mant(48), .f_exp(8), .f_mant(15))::rtof(-0.125);
+        reCon = convert_nonsynth#(.n_int(16), .n_mant(48), .f_exp(8), .f_mant(15))::ftor(tempResult);
+        $display("Dummy test: sign %1b exp %d mant %h = %f", tempResult.sign, tempResult.exp, tempResult.mant, reCon);
+        
+        tempResult = convert#(.n_int(16), .n_mant(48), .f_exp(8), .f_mant(15))::itof(-1 <<< 45);
+        reCon = convert_nonsynth#(.n_int(16), .n_mant(48), .f_exp(8), .f_mant(15))::ftor(tempResult);
+        $display("Float: sign %1b exp %d mant %h = %f", tempResult.sign, tempResult.exp, tempResult.mant, reCon);
+    end
+    /**/
     
     // Define clock
     initial begin
@@ -139,13 +205,12 @@ module TB_FIR #() ();
     end
 
     // Instantiate DUTs
-    FIR_top #(.Lookahead(`LOOKAHEAD), .Lookback(`LOOKBACK), .OSR(`OSR)) DUT_FIR (
-            .in(inSample), .rst(rst), .clk(clk), .out(dutResult)); 
-    
+    FIR_top #(.Lookahead(`LOOKAHEAD), .Lookback(`LOOKBACK), .OSR(`OSR), .n_exp(`EXP_W), .n_mant(`MANT_W)) DUT_FIR (
+            .in(inSample), .rst(rst), .clk(clk), .out(dutResult), .valid(isValid)); 
     
     // Bind Modules to property checkers
-    bind FPU FPU_prop #(.op(op)) flprop_i (.*);
-    bind LUT LUT_prop #(.size(size), .fact(fact)) lutprop_i (.*);
-    bind FIR_top FIR_prop #(.Lookahead(Lookahead), .Lookback(Lookback), .OSR(OSR)) firprop_i (.*);
+    //bind FPU FPU_prop flprop_i (.*);
+    //bind LUT LUT_prop #(.size(size), .fact(fact)) lutprop_i (.*);
+    //bind FIR_top FIR_prop #(.Lookahead(Lookahead), .Lookback(Lookback), .OSR(OSR)) firprop_i (.*);
 
 endmodule
