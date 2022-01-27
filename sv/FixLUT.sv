a25941cc690ad64af859767e84613b34dc382f78
GuavTek <ddjava@live.no>
2022-01-27 17:12:17 +0100

    Cleanup

--- sv/FixLUT.sv
+++ sv/FixLUT.sv
@@ -16,12 +16,11 @@ module FixLUT #(
     localparam n_tot = n_int + n_mant;
     input logic[size-1:0] sel;
     output logic signed[n_tot:0] result;
-    // Generate LUT values
     logic signed[n_tot:0] mem[2**size-1:0];
 
     function automatic logic signed[n_tot:0] getVal(logic[size-1:0] in);
         logic signed[n_tot:0] temp = 0;
-        logic[size:0] j;
+        int j;
         for(j = 0; j < size; j++) begin
             if(in[j] == 1) begin
                 temp += fact[j][n_tot:0];
@@ -32,15 +31,14 @@ module FixLUT #(
         return temp;
     endfunction
 
-    genvar i;
+    // Generate LUT values
     generate
-        for(i = 0; i < 2**size; i++) begin
+        for(genvar i = 0; i < 2**size; i++) begin
             localparam logic signed[n_tot:0] temp = getVal(i);
             assign mem[i] = temp;
         end
     endgenerate
 
-    
     assign result = mem[sel];
 endmodule
 
@@ -94,21 +92,21 @@ module FixLUT_Unit #(
         GetFirstReg = temp;
     endfunction
 
-    localparam LUTRest = size % lut_size;
+    localparam int LUTRest = size % lut_size;
     function logic signed[lut_size-1:0][n_tot:0] GetFact (int startIndex);
         logic signed[lut_size-1:0][n_tot:0] tempArray;
             
         for (int i = 0; i < lut_size ; i++) begin
-            tempArray[i][n_tot:0] = fact[startIndex + i][n_tot:0];
+            tempArray[i] = fact[startIndex + i];
         end
         return tempArray;
-    endfunction
+    endfunction 
 
     function logic signed[LUTRest-1:0][n_tot:0] GetFactRest (int startIndex);
         logic signed[LUTRest-1:0][n_tot:0] tempArray;
             
         for (int i = 0; i < LUTRest ; i++) begin
-            tempArray[i][n_tot:0] = fact[startIndex + i][n_tot:0];
+            tempArray[i] = fact[startIndex + i];
         end
         return tempArray;
     endfunction
@@ -124,10 +122,11 @@ module FixLUT_Unit #(
             localparam lut_rem = size - offset;
             if (i < $floor(size/lut_size)) begin
                 localparam logic signed[lut_size-1:0][n_tot:0] fact_slice = GetFact(offset);
-                FixLUT #(.size(lut_size), .n_int(n_int), .n_mant(n_mant), .fact(fact_slice)) lut_ (.sel(sel[offset +: lut_size]), .result(tempResult));
-            end else if (lut_rem > 0) begin
-                localparam logic signed[lut_rem-1:0][n_tot:0] fact_slice = GetFactRest(offset);
-                FixLUT #(.size(lut_rem), .n_int(n_int), .n_mant(n_mant), .fact(fact_slice)) lut_ (.sel(sel[offset +: lut_rem]), .result(tempResult));
+                FixLUT #(.size(lut_size), .n_int(n_int), .n_mant(n_mant), .fact(fact_slice)) lut_f (.sel(sel[offset +: lut_size]), .result(tempResult));
+            end else begin
+                // Causes internal assertion error in synthesis tool?
+                localparam logic signed[LUTRest-1:0][n_tot:0] fact_slice = GetFactRest(offset);
+                FixLUT #(.size(LUTRest), .n_int(n_int), .n_mant(n_mant), .fact(fact_slice)) lut_r (.sel(sel[offset +: LUTRest]), .result(tempResult));
             end
 
             if (lut_comb > 0) begin : Comb_Gen
