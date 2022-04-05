9f06e07117b9f8345a977378b2342e9e1437b18d
GuavTek <ddjava@live.no>
2022-04-05 17:52:05 +0200

    Split rst to an async rst, and sync load

--- sv/Recursion_Fxp.sv
+++ sv/Recursion_Fxp.sv
@@ -4,7 +4,7 @@
 `include "CFxpPU.sv"
 `include "Util.sv"
 
-// A general recursive calculation
+// A general recursive calculation with complex numbers
 // Performs out = in[k] + factor*in[k-1]
 module Recursion_Fxp #(
                 n_int = 8,
@@ -13,34 +13,37 @@ module Recursion_Fxp #(
                                             factorI = 0
 ) (
     input logic signed[n_int+n_mant:0] inR, inI,
-    input logic signed[n_int+n_mant:0] resetValR, resetValI,
-    input logic rst, clk,
+    input logic signed[n_int+n_mant:0] loadValR, loadValI,
+    input logic rst, clk, load,
     output logic signed[n_int+n_mant:0] outR, outI
 );
-    logic signed[n_int+n_mant:0] prodR, prodI, sumR, sumI, prevR, prevI, prevSumR, prevSumI;
-    logic resetting;
-    assign outR = prevSumR;
-    assign outI = prevSumI;
-    assign prevR = resetting ? prevSumR : resetValR;
-    assign prevI = resetting ? prevSumI : resetValI;
+    logic signed[n_int+n_mant:0] prodR, prodI, sumR, sumI, prevR, prevI, nextR, nextI;
+    assign outR = sumR;
+    assign outI = sumI;
 
     CFxpPU #(.op(FPU_p::MULT), .n_int(n_int), .n_mant(n_mant)) c1 (.AR(prevR), .AI(prevI), .BR(factorR), .BI(factorI), .clk(clk), .resultR(prodR), .resultI(prodI));
     CFxpPU #(.op(FPU_p::ADD), .n_int(n_int), .n_mant(n_mant)) c2 (.AR(prodR), .AI(prodI), .BR(inR), .BI(inI), .clk(clk), .resultR(sumR), .resultI(sumI));
 
-    // Hold rst until next clock edge
-    always_latch begin : rst_latch
+    always @(posedge clk, negedge rst) begin : recurseR
         if (!rst)
-            resetting <= 0;
-        else if (clk)
-            resetting <= 1;
+            prevR <= 'b0;
+        else if (!load)
+            prevR <= loadValR;
+        else
+            prevR <=  sumR;
     end
 
-    always @(posedge clk) begin : recurse
-        prevSumR <= sumR;
-        prevSumI <= sumI;
+    always @(posedge clk, negedge rst) begin : recurseI
+        if (!rst)
+            prevI <= 'b0;
+        else if (!load)
+            prevI <= loadValI;
+        else
+            prevI <=  sumI;
     end
 
 endmodule
+
 
 // A module for recursive lookback calculation
 module LookbackRecursion #(
