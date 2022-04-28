b777c59bcef3976d3cc6b7d9aba86ab7a9557236
GuavTek <ddjava@live.no>
2022-04-28 17:27:30 +0200

    Add simulation with different master frequencies

--- tb/simulate.py
+++ tb/simulate.py
@@ -125,7 +125,7 @@ if plotResults:
         label = topName + f" with format {exp}p{mant}, {depth} lookahead length, and DSR={DSR}"
     elif topModule.find('Hybrid') != -1:
         label = topName + f" with format {exp}p{mant}, {depth} lookahead length, and DSR={DSR}"
-    SNR = plot.PlotFigure(res[int(math.ceil(8*freq/DSR)):int(-8*freq/DSR)], int(4*freq/DSR), label, outfileName, freq*1e6/DSR)
+    SNR = plot.PlotFigure(res[int(math.ceil(8*freq/DSR)):int(-8*freq/DSR)], int(2*freq/DSR), label, outfileName, freq*1e6/DSR)
     
     f = open('Data/' + outfileName + '_SNR.txt', "w")
     f.write(str(SNR) + '\n\n')
