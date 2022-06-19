df7261338c1fc52902228c969d99cfcbf3a93516
GuavTek <ddjava@live.no>
2022-06-19 17:27:21 +0200

    Switch the order of rounding and signing

--- sv/Flp_To_Fxp.sv
+++ sv/Flp_To_Fxp.sv
@@ -21,18 +21,18 @@ module Flp_To_Fxp #(
     logic[n_mant_in:0] num_in;
     assign num_in = (in.exp == 0) ? {1'b0, in.mant} : {1'b1, in.mant};
 
-    logic signed[n_mant_in+1:0] num_signed;
-    assign num_signed = in.sign ? -num_in : num_in;
+    logic signed[n_tot_out:0] num_signed;
+    assign num_signed = in.sign ? -temp[n_tot_out+1:1] : temp[n_tot_out+1:1];
 
     logic signed[n_exp_in:0] exponent;
     assign exponent = in.exp - expBias;
 
     logic signed[n_exp_in+1:0] shift;
-    assign shift = n_mant_in - n_mant_out - exponent -1;
+    assign shift = n_mant_in - n_mant_out - exponent - 1;
 
-    assign temp = ((shift < 0) ? num_signed <<< -shift : num_signed >>> shift) + 1;
+    assign temp = ((shift < 0) ? num_in << -shift : num_in >> shift) + 1;
 
-    assign out = temp[n_tot_out+1:1];
+    assign out = num_signed;
 
 endmodule
 
